c02196b2f31538070bbc1c779f1b89476e7d3429
Lucas Eduardo <122051482+DevM0rales@users.noreply.github.com>
2025-12-10 15:49:31 -0300

    Add static directories for usuarios and products apps
    Updated STATICFILES_DIRS in settings.py to include static directories for 'usuarios' and 'products' apps, in addition to 'nucleo'. This ensures static files from all relevant apps are collected. Minor formatting change in Procfile (added newline).

--- Nerdhub/settings.py
+++ Nerdhub/settings.py
@@ -131,7 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'nucleo/static')]
+STATICFILES_DIRS = [
+    BASE_DIR / "nucleo" / "static",
+    BASE_DIR / "usuarios" / "static",
+    BASE_DIR / "products" / "static",
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
